8a919af73b2c668ab7fce78c4b78e9d3a2b9d6e8
kevinschellingerbastion <kevin.schellinger.bastion@gmail.com>
2026-09-10 21:32:00 +0000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { prisma } from "@/lib/prisma";
+//import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
-  const count = await prisma.amenity.count();
+  //const count = await prisma.amenity.count();
 
   return (
     <main>
       <h1>groepshuizen.nl</h1>
-      <p>Aantal faciliteiten: {count}</p>
+      <p>Aantal faciliteiten: </p>
     </main>
   );
 }
